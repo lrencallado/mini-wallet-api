cbced73fe4a998b98a6f08c8a39f31a1e2a4c4e8
Lloyd Encallado <lloydencallado@gmail.com>
2025-11-14 01:26:20 +0800

    handled cors for api and broadcasting routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->api(prepend: [
+            \Illuminate\Http\Middleware\HandleCors::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         // Optional: Add custom logging for specific exceptions if needed
